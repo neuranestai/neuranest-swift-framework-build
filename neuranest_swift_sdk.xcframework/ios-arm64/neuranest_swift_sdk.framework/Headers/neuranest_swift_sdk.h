657c21414ddeb366c3ad6d5f40ac592f2a0f6247
Ahmed Shoukr <ahmedshoukr10@gmail.com>
2023-09-04 16:23:10 -0700

    first commit

---
+++ neuranest_swift_sdk.xcframework/ios-arm64/neuranest_swift_sdk.framework/Headers/neuranest_swift_sdk.h
@@ -0,0 +1,18 @@
+//
+//  neuranest_swift_sdk.h
+//  neuranest-swift-sdk
+//
+//  Created by Ahmed on 8/30/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for neuranest_swift_sdk.
+FOUNDATION_EXPORT double neuranest_swift_sdkVersionNumber;
+
+//! Project version string for neuranest_swift_sdk.
+FOUNDATION_EXPORT const unsigned char neuranest_swift_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <neuranest_swift_sdk/PublicHeader.h>
+
+
